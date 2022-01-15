937e074b44353e95276da3572d72ef1cbe9c7327
francobenitez <franco.benitez@hogarthww.com>
2022-01-15 15:30:49 -0300

    feat: add latest products from the django api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="home">
     Home
+
+    <div class="" v-for="product in latestProducts" :key="product.id">
+       <div class="">
+        <figure>
+          <img :src="product.get_thumbnail">
+        </figure>
+
+        <h3>{{ product.name }}</h3>
+        <p class="">${{ product.price }}</p>
+
+        View details
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
+  data() {
+    return {
+      latestProducts: []
+    }
+  },
+  mounted() {
+    this.getLatestProducts()
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get('/api/v1/latest-products/')
+        .then(response => {
+          this.latestProducts = response.data
+        }).catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
